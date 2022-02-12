b0d4e462309517883c71c5079640d4747535316d
Ildar <yafarov13@gmail.com>
2022-02-12 15:58:19 +0300

    Homework7

---
+++ homework7_3/Repository/Product.php
@@ -0,0 +1,126 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Model\Repository;
+
+use Model\Entity;
+
+class Product
+{
+
+    private $identityMap = [];
+
+    private function getGlobalKey(int $id, string $name)
+    {
+        return sprintf('%d.%s',
+            $id,
+            $name);
+    }
+    /**
+     * Поиск продуктов по массиву id
+     *
+     * @param int[] $ids
+     * @return Entity\Product[]
+     */
+    public function search(array $ids = []): array
+    {
+        if (!count($ids)) {
+            return [];
+        }
+        $product = $this->getDataFromSource($ids);
+        $key = $this->getGlobalKey($product['id'], $product['name']);
+
+        if (isset($this->identityMap[$key])) {
+            return $this->identityMap[$key];
+        } else {
+            foreach ($this->getDataFromSource(['id' => $ids]) as $item) {
+                $this->identityMap[$key] = new Entity\Product($item['id'], $item['name'], $item['price']);
+            }
+            return $this->identityMap[$key];
+        }
+    }
+
+    /**
+     * Получаем все продукты
+     *
+     * @return Entity\Product[]
+     */
+    public function fetchAll(): array
+    {
+        $productList = [];
+        foreach ($this->getDataFromSource() as $item) {
+            $productList[] = new Entity\Product($item['id'], $item['name'], $item['price']);
+        }
+
+        return $productList;
+    }
+
+    /**
+     * Получаем продукты из источника данных
+     *
+     * @param array $search
+     *
+     * @return array
+     */
+    private function getDataFromSource(array $search = [])
+    {
+        $dataSource = [
+            [
+                'id' => 1,
+                'name' => 'PHP',
+                'price' => 15300,
+            ],
+            [
+                'id' => 2,
+                'name' => 'Python',
+                'price' => 20400,
+            ],
+            [
+                'id' => 3,
+                'name' => 'C#',
+                'price' => 30100,
+            ],
+            [
+                'id' => 4,
+                'name' => 'Java',
+                'price' => 30600,
+            ],
+            [
+                'id' => 5,
+                'name' => 'Ruby',
+                'price' => 18600,
+            ],
+            [
+                'id' => 8,
+                'name' => 'Delphi',
+                'price' => 8400,
+            ],
+            [
+                'id' => 9,
+                'name' => 'C++',
+                'price' => 19300,
+            ],
+            [
+                'id' => 10,
+                'name' => 'C',
+                'price' => 12800,
+            ],
+            [
+                'id' => 11,
+                'name' => 'Lua',
+                'price' => 5000,
+            ],
+        ];
+
+        if (!count($search)) {
+            return $dataSource;
+        }
+
+        $productFilter = function (array $dataSource) use ($search): bool {
+            return in_array($dataSource[key($search)], current($search), true);
+        };
+
+        return array_filter($dataSource, $productFilter);
+    }
+}
